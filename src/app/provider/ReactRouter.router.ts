ff2d41518291a05fa6c0d3ef672ec01bc83c3193
kr-younghoon <sullanta0802@gmail.com>
2025-08-19 10:46:33 +0900

    feat: 라우터 및 API 관련 변경 사항
    - ReactRouter.router.ts에서 기본 경로를 MainPage로 변경하고, '/api-test' 경로를 추가하여 ApiTestPage를 설정했습니다.
    - echo.api.ts에서 echo 함수와 관련된 코드를 제거하고 health 함수만 남겼습니다.
    - types.ts에서 EchoRequest 및 EchoResponse 타입을 삭제했습니다.
    - ApiTestPage에서 echo 호출 관련 버튼을 제거하고 health 호출 버튼만 남겼습니다.
    - main-page.tsx 파일을 새로 추가했습니다.

--- src/app/provider/ReactRouter.router.ts
+++ src/app/provider/ReactRouter.router.ts
@@ -2,12 +2,17 @@ import React from 'react';
 
 import { createBrowserRouter } from 'react-router-dom';
 import ApiTestPage from '../../pages/test/api-test-page';
+import MainPage from '../../pages/main/main-page';
 
 const router = createBrowserRouter([
-  {
-    path: '/',
-    element: React.createElement(ApiTestPage),
-  },
+    {
+        path: '/api-test',
+        element: React.createElement(ApiTestPage),
+    },
+    {
+        path: '/',
+        element: React.createElement(MainPage),
+    },
 ]);
 
 export default router;
